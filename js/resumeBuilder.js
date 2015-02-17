5463b09017cf9d22b031ae7c0567f30ba5098eba
Kristian Kanya <kristiankanya@Kristians-MBP.att.net>
2015-02-16 23:14:11 -0600

    google maps working, images added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,17 +7,17 @@ var bio = {
       "role": "Web Developer",
       "contacts":
       {
-        "mobile": "555-555-5555",
+        "mobile": "916-802-0552",
         "email": "kriskanya@gmail.com",
         "github": "github.com/kriskanya",
         "twitter": "twitter.com/kriskanya",
         "location": "Nashville, TN"
       },
-      "skills": ["HTML", "CSS", "JavaScript"],
-      "biopic": "http://purrfectcatnames.com/wp-content/uploads/2013/11/Gray-Cat-MorgueFile-Nov16th-2013.jpg",
+      "skills": ["HTML", "CSS", "JavaScript", "Ruby", "Testing"],
+      "biopic": "images/profile.jpg",
     }
   ],
-  "display": function() {
+  display: function() {
     // name and role
     var formattedName = HTMLheaderName.replace('%data%', bio.bio[0].name);
     var formattedRole = HTMLheaderRole.replace('%data%', bio.bio[0].role);
@@ -69,7 +69,7 @@ var education = {
       "url": "http://www.udacity.com"
     }
   ],
-  "display": function() {
+  display: function() {
     // education
     $('#education').append(HTMLschoolStart);
 
@@ -113,7 +113,7 @@ var work = {
       "location": "Nashville, TN",
       "dates": "November 2014 - Present",
       "description": "I mainly write end-to-end tests for the Angular apps using the Protractor testing framework and unit tests with Jasmine.  " +
-                     "The different NextGxDx apps are written using the MEAN stack, with some of the backend in C#."
+                     "The different NextGxDx apps are written using the MEAN stack."
     },
     {
       "employer": "Hulu",
@@ -123,7 +123,7 @@ var work = {
       "description": "Implemented advertising campaigns worth over $8 million per quarter, integrated third-party tracking, and delegated Creative Services tasks and timelines via JIRA."
     }
   ],
-  "display": function() {
+  display: function() {
     // work experience
     $('#workExperience').append(HTMLworkStart);
 
@@ -150,22 +150,21 @@ var projects = {
       "link": "https://github.com/kriskanya/udacity-project1",
       "images":
       [
-        "http://www.adweek.com/fishbowldc/files/2010/12/hoodie-red-cat.jpg",
-        "http://www.adweek.com/fishbowldc/files/2010/12/hoodie-red-cat.jpg"
+        "images/Project_1.png"
       ]
     },
     {
       "title": "MOOChub",
       "dates": "September 2014",
-      "description": "Reddit-style site for users to upload their favorite online courseware and comment on them.",
+      "description": "Reddit-style site for users to upload their favorite massively open online courses (MOOCs), and vote and comment on them.",
       "link": "http://moochub.herokuapp.com",
       "images":
       [
-        "http://breadedcat.com/wp-content/uploads/2012/02/cat-breading-tutorial-004.jpg"
+        "images/moochub.png"
       ]
     }
   ],
-  "display": function() {
+  display: function() {
     // projects
     $('#projects').append(HTMLprojectStart);
 
@@ -198,121 +197,15 @@ function populateBio(divId) {
   $(divId).append(locationContact);
 }
 
-bio.display();
+// Function calls
 
-// // name and role
-// var formattedName = HTMLheaderName.replace('%data%', bio.bio[0].name);
-// var formattedRole = HTMLheaderRole.replace('%data%', bio.bio[0].role);
-// $('#header').prepend(formattedRole);
-// $('#header').prepend(formattedName);
-//
-// // contact info
-// var mobileContact = HTMLmobile.replace('%data%', bio.bio[0].contacts.mobile);
-// var emailContact = HTMLemail.replace('%data%', bio.bio[0].contacts.email);
-// var githubContact = HTMLgithub.replace('%data%', bio.bio[0].contacts.github);
-// var twitterContact = HTMLtwitter.replace('%data%', bio.bio[0].contacts.twitter);
-// var locationContact = HTMLlocation.replace('%data%', bio.bio[0].contacts.location);
-// $('#topContacts').append(mobileContact);
-// $('#topContacts').append(emailContact);
-// $('#topContacts').append(githubContact);
-// $('#topContacts').append(twitterContact);
-// $('#topContacts').append(locationContact);
-//
-// var bioPic = HTMLbioPic.replace('%data%', bio.bio[0].biopic);
-// $('#header').append(bioPic);
-//
-// // skills
-// $('#header').append(HTMLskillsStart);
-//
-// for (skillIndex in bio.bio[0].skills) {
-//   var skill = HTMLskills.replace('%data%', bio.bio[0].skills[skillIndex]);
-//   $('#skills').append(skill);
-// }
+bio.display();
 
 work.display();
 
-// // work experience
-// $('#workExperience').append(HTMLworkStart);
-//
-// for (jobIndex in work.jobs) {
-//   var workEmployer = HTMLworkEmployer.replace('%data%', work.jobs[jobIndex].employer);
-//   var workTitle = HTMLworkTitle.replace('%data%', work.jobs[jobIndex].title);
-//   var workDates = HTMLworkDates.replace('%data%', work.jobs[jobIndex].dates);
-//   var workLocation = HTMLworkLocation.replace('%data%', work.jobs[jobIndex].location);
-//   var workDescription = HTMLworkDescription.replace('%data%', work.jobs[jobIndex].description);
-//   $('.work-entry:last').append(workEmployer + workTitle);
-//   $('.work-entry:last').append(workDates);
-//   $('.work-entry:last').append(workLocation);
-//   $('.work-entry:last').append(workDescription);
-// }
-
 projects.display();
-
-// // projects
-// $('#projects').append(HTMLprojectStart);
-//
-// for (projectIndex in projects.projects) {
-//   var projectTitle = HTMLprojectTitle.replace('%data%', projects.projects[projectIndex].title).replace('%link%', projects.projects[projectIndex].link);
-//   var projectDates = HTMLprojectDates.replace('%data%', projects.projects[projectIndex].dates);
-//   var projectDescription = HTMLprojectDescription.replace('%data%', projects.projects[projectIndex].description);
-//   $('.project-entry:last').append(projectTitle);
-//   $('.project-entry:last').append(projectDates);
-//   $('.project-entry:last').append(projectDescription);
-//
-//   for (var i = 0; i < projects.projects[projectIndex].images.length; i++) {
-//     var projectImage = HTMLprojectImage.replace('%data%', projects.projects[projectIndex].images[i]);
-//     $('.project-entry:last').append(projectImage);
-//   }
-// }
 
 education.display();
 
-// // education
-// $('#education').append(HTMLschoolStart);
-//
-// for (courseIndex in education.schools) {
-//   var schoolName = HTMLschoolName.replace('%data%', education.schools[courseIndex].name).replace('%link%', education.schools[courseIndex].url);
-//   var schoolDegree = HTMLschoolDegree.replace('%data%', education.schools[courseIndex].degree);
-//   var schoolDates = HTMLschoolDates.replace('%data%', education.schools[courseIndex].dates);
-//   var schoolLocation = HTMLschoolLocation.replace('%data%', education.schools[courseIndex].location);
-//   $('.education-entry:last').append(schoolName + schoolDegree);
-//   $('.education-entry:last').append(schoolDates);
-//   $('.education-entry:last').append(schoolLocation);
-//
-//   for (var i = 0; i < education.schools[courseIndex].majors.length; i++) {
-//     var schoolMajor = HTMLschoolMajor.replace('%data%', education.schools[courseIndex].majors[i]);
-//     $('.education-entry:last').append(schoolMajor);
-//   }
-// }
-//
-// // online education
-// $('#education').append(HTMLonlineClasses);
-// $('#education').append(HTMLonlineClassesDiv);
-//
-// for (courseIndex in education.onlineCourses) {
-//   var onlineTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[courseIndex].title);
-//   var onlineSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[courseIndex].school);
-//   var onlineDates = HTMLonlineDates.replace('%data%', education.onlineCourses[courseIndex].dates);
-//   var onlineURL = HTMLonlineURL.replace('%data%', education.onlineCourses[courseIndex].url).replace(
-//     '%link%', education.onlineCourses[courseIndex].url
-//   );
-//   $('.online-education-entry').append(onlineTitle + onlineSchool);
-//   $('.online-education-entry').append(onlineDates);
-//   $('.online-education-entry').append(onlineURL);
-// }
-
 // map
-
 $('#mapDiv').append(googleMap);
-
-// // lets connect
-// var mobileContact = HTMLmobile.replace('%data%', bio.bio[0].contacts.mobile);
-// var emailContact = HTMLemail.replace('%data%', bio.bio[0].contacts.email);
-// var githubContact = HTMLgithub.replace('%data%', bio.bio[0].contacts.github);
-// var twitterContact = HTMLtwitter.replace('%data%', bio.bio[0].contacts.twitter);
-// var locationContact = HTMLlocation.replace('%data%', bio.bio[0].contacts.location);
-// $('#footerContacts').append(mobileContact);
-// $('#footerContacts').append(emailContact);
-// $('#footerContacts').append(githubContact);
-// $('#footerContacts').append(twitterContact);
-// $('#footerContacts').append(locationContact);
